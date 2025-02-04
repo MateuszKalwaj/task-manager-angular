0865296ba546bfd5ebb04dfd7756d5697ac0a102
mateuszkalwaj <mateusz.kalwaj@gmail.com>
2025-02-04 14:49:45 +0100

    Header Component added

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import {HeaderComponent} from './app/header.component';
+import { AppComponent } from './app/app.component';
 
-bootstrapApplication(HeaderComponent).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));

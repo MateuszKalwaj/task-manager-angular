fe30336546d7135a7d734db0dc49036cd939ac66
mateuszkalwaj <mateusz.kalwaj@gmail.com>
2025-02-05 13:29:40 +0100

    Property binding and String interpolation on userComponent

---
+++ src/app/dummy-users.ts
@@ -0,0 +1,32 @@
+export const DUMMY_USERS = [
+  {
+    id: 'u1',
+    name: 'Janina Niezbyt',
+    avatar: 'user-1.jpg',
+  },
+  {
+    id: 'u2',
+    name: 'Emilia Silna',
+    avatar: 'user-2.jpg',
+  },
+  {
+    id: 'u3',
+    name: 'Marek Kanarek',
+    avatar: 'user-3.jpg',
+  },
+  {
+    id: 'u4',
+    name: 'Dawid Mondry',
+    avatar: 'user-4.jpg',
+  },
+  {
+    id: 'u5',
+    name: 'Czesława Mobilna',
+    avatar: 'user-5.jpg',
+  },
+  {
+    id: 'u6',
+    name: 'Ariel Kosmaty',
+    avatar: 'user-6.jpg',
+  },
+];

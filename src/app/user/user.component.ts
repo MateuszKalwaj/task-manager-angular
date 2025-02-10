4906a5d18c4fa63426ddc4d82a6ba4e1d18b2d34
mateuszkalwaj <mateusz.kalwaj@gmail.com>
2025-02-10 14:36:34 +0100

    @for to iterate list items

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,17 +1,20 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 
+interface User {
+  id: string,
+  name: string,
+  avatar: string
+}
+
 @Component({
   selector: 'app-user',
   imports: [],
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css'],
 })
+
 export class UserComponent {
-  @Input({required:true}) user!: {
-    id: string,
-    name: string,
-    avatar: string
-}
+  @Input({required:true}) user!: User;
 
   @Output() select = new EventEmitter<string>();
 

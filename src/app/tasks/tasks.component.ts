9c2a115443fe7bd487cb3c59b836d007292540d8
mateuszkalwaj <mateusz.kalwaj@gmail.com>
2025-02-12 16:27:49 +0100

    added dummy new-task after click

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,10 +1,12 @@
 import {Component, Input} from '@angular/core';
 import {TaskComponent} from './task/task.component';
+import {NewTaskComponent} from './new-task/new-task.component';
 
 @Component({
   selector: 'app-tasks',
   imports: [
-    TaskComponent
+    TaskComponent,
+    NewTaskComponent
   ],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
@@ -12,6 +14,7 @@ import {TaskComponent} from './task/task.component';
 export class TasksComponent {
   @Input({required:true}) userId!: string;
   @Input({required:true}) name!: string;
+  isAddingTask = false;
 
   tasks = [
     {
@@ -45,5 +48,9 @@ export class TasksComponent {
 
   onCompleteTask(id: string) {
     this.tasks = this.tasks.filter((task)=> task.id !== id);
+  }
+
+  onAddTask() {
+    this.isAddingTask = true;
   }
 }

88e1fb426ea68947523289f2a20e48f5230676d1
mateuszkalwaj <mateusz.kalwaj@gmail.com>
2025-02-12 15:56:11 +0100

    dynamic CSS styling with class binding

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,10 +1,5 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
-
-interface User {
-  id: string,
-  name: string,
-  avatar: string
-}
+import {User} from './user.model';
 
 @Component({
   selector: 'app-user',
@@ -15,7 +10,7 @@ interface User {
 
 export class UserComponent {
   @Input({required:true}) user!: User;
-
+  @Input({required: true}) selected!: boolean
   @Output() select = new EventEmitter<string>();
 
   get imagePath() {
